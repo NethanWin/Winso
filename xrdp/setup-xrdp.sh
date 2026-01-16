a0a154ddf97ab9e1d14b0e99c8d16c87d79d65d4
NethanWin <nethan.win@protonmail.com>
2026-01-16 12:48:08 +0200

    xrdo

---
+++ xrdp/setup-xrdp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo mkdir -p /etc/polkit-1/localauthority/90-mandatory.d
+sudo mv 99-xrdp.pkla /etc/polkit-1/localauthority/90-mandatory.d/
+
+sudo mv startwm.sh /etc/xrdp/startwm.sh
