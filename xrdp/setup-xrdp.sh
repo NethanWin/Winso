3633384bf7b3aeb830e737ebbf97cecfc03ffe38
NethanWin <nethan.win@protonmail.com>
2026-01-16 19:09:15 +0200

    xrdp 3

--- xrdp/setup-xrdp.sh
+++ xrdp/setup-xrdp.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 sudo mkdir -p /etc/polkit-1/localauthority/90-mandatory.d
-sudo mv 99-xrdp.pkla /etc/polkit-1/localauthority/90-mandatory.d/
+sudo cp 99-xrdp.pkla /etc/polkit-1/localauthority/90-mandatory.d/
 
-sudo mv startwm.sh /etc/xrdp/startwm.sh
+sudo cp startwm.sh /etc/xrdp/startwm.sh
+
+cp .xsession $HOME/.xsession
 
 sudo systemctl restart xrdp xrdp-sesman
